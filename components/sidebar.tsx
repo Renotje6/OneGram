87f75c18e8da72cfac3ecfe86e2cf05236a2c134
Default <j.rehmannski@gmail.com>
2024-06-26 13:06:34 +0200

    fixed sidebars

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -10,35 +10,37 @@ interface SideBarProps {}
 
 const SideBar: FC<SideBarProps> = ({}) => {
 	return (
-		<nav className='bg-black/20 text-black p-4 min-h-screen w-80 flex-col gap-5 justify-between shadow-lg hidden lg:flex min-w-80 fixed right-0'>
-			<div className='h-full'>
-				<p className='text-lg font-semibold dark:text-zinc-300'>YOUR FOLLOWERS</p>
-				<div className='p-2 flex flex-col gap-2 items-start'>
-					<SideBarUser
-						following
-						id='jane-doe'
-						name='Jane Doe'
-						lastseen='5h ago'
-						image='https://i.pravatar.cc/150?u=a04258114e29026702d'
+		<nav className='bg-black/20 text-black min-h-screen w-80 flex-col gap-5 justify-between shadow-lg hidden lg:flex min-w-80'>
+			<div className='h-screen p-4 flex justify-between flex-col fixed gap-5 w-80'>
+				<div className='h-full'>
+					<p className='text-lg font-semibold dark:text-zinc-300'>YOUR FOLLOWERS</p>
+					<div className='p-2 flex flex-col gap-2 items-start'>
+						<SideBarUser
+							following
+							id='jane-doe'
+							name='Jane Doe'
+							lastseen='5h ago'
+							image='https://i.pravatar.cc/150?u=a04258114e29026702d'
+						/>
+					</div>
+				</div>
+				<div>
+					<Button
+						as={Link}
+						size='lg'
+						variant='light'
+						href={`/logout`}
+						className='flex justify-between w-full items-center p-2 text-black dark:text-zinc-300'
+						endContent={<TbLogout className='size-6 dark:text-zinc-400' />}
+						startContent={
+							<User
+								name='Logout'
+								avatarProps={{ src: '', showFallback: true }}
+								classNames={{ description: 'text-zinc-500', name: 'font-semibold dark:text-zinc-300 text-lg' }}
+							/>
+						}
 					/>
 				</div>
-			</div>
-			<div>
-				<Button
-					as={Link}
-					size='lg'
-					variant='light'
-					href={`/logout`}
-					className='flex justify-between w-full items-center p-2 text-black dark:text-zinc-300'
-					endContent={<TbLogout className='size-6 dark:text-zinc-400' />}
-					startContent={
-						<User
-							name='Logout'
-							avatarProps={{ src: '', showFallback: true }}
-							classNames={{ description: 'text-zinc-500', name: 'font-semibold dark:text-zinc-300 text-lg' }}
-						/>
-					}
-				/>
 			</div>
 		</nav>
 	);
