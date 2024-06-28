4462e8a3a26b3995f828a77d96bdede435a479a4
Renotje6 <reno.rovers@gmail.com>
2024-06-28 12:02:37 +0200

    setup jest for testing

---
+++ __tests__/navbar.test.tsx
@@ -0,0 +1,28 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import NavBar from '@/components/navbar'; // Adjust the path accordingly
+import '@testing-library/jest-dom/';
+import { render, screen } from '@testing-library/react';
+
+describe('NavBar Component', () => {
+	test('renders NavBar with links', () => {
+		render(<NavBar />);
+
+		// Check if the main title is rendered
+		expect(screen.getByText('OneGram')).toBeInTheDocument();
+
+		// Check if the HOME link is rendered
+		expect(screen.getByText('HOME')).toBeInTheDocument();
+
+		// Check if the SEARCH link is rendered
+		expect(screen.getByText('SEARCH')).toBeInTheDocument();
+
+		// Check if the Profile link is rendered
+		expect(screen.getByText('Profile')).toBeInTheDocument();
+
+		// Check if the Settings link is rendered
+		expect(screen.getByText('Settings')).toBeInTheDocument();
+	});
+});
