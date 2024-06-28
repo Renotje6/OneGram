69b8bbb4c303e317e83656a46e210f3e1cade1a8
Renotje6 <reno.rovers@gmail.com>
2024-06-28 22:47:57 +0200

    update

--- __tests__/e2e/register.test.ts
+++ __tests__/e2e/register.test.ts
@@ -19,9 +19,9 @@ test('Should be able to register', async ({ page }) => {
 test('Error should be shown when email is already in use', async ({ page }) => {
 	await page.goto('http://localhost:3000/register');
 
-	await page.fill('input[name="name"]', 'John Doe');
-	await page.fill('input[name="email"]', `john.doe${randomNumber}@mail.com`);
-	await page.fill('input[name="password"]', 'password');
+	await page.fill('input[name="name"]', 'Test');
+	await page.fill('input[name="email"]', `test.test@com`);
+	await page.fill('input[name="password"]', 'test1234');
 
 	await page.click('button[name="submit"]');
 	await page.waitForSelector('text=Email already in use');
